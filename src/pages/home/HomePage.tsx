cfcbe2379c964c2fd99470ff6f65a0a60058b3d7
windukeav <windukeav@gmail.com>
2020-08-10 16:43:21 +0300

    problem with last border last button

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -13,13 +13,59 @@ export const HomePage: React.FC<HomePageProps> = ({ }) => {
   return (
     <section>
       <div  style={{  }}>
-        <Button onClick={handleButtonClick}>title</Button>
+        <Button onClick={handleButtonClick}>Что делать, если я не умею позировать</Button>
         <Collapse isCollapsed={isCollapsed}>
-          <h1 style={{  }}>content</h1>
+          <h1 style={{  }}>
+            Выставить человека в кадре - задача фотографа, а не модели. Это не Ваша "головная боль", а моя. 
+          </h1>
         </Collapse>
-        <Button onClick={handleButtonClick}>title</Button>
+        
+        <Button onClick={handleButtonClick} >
+          Могу ли я использовать фотографии с одной съемки?
+        </Button>
+
         <Collapse isCollapsed={isCollapsed}>
-          <h1 style={{  }}>content</h1>         
+          <h1 style={{  }}>
+            
+          </h1>
+        </Collapse>
+
+        <Button onClick={handleButtonClick}>
+          Как правильно придумывать сценарий съемки?
+        </Button>
+
+        <Collapse isCollapsed={isCollapsed}>
+          <h1 style={{  }}>
+          
+          </h1>
+        </Collapse>
+
+        <Button onClick={handleButtonClick}>
+          У меня была профессиональная фотосессия и результат меня разочаровал
+        </Button>
+
+        <Collapse isCollapsed={isCollapsed}>
+          <h1 style={{  }}>
+            
+          </h1>
+        </Collapse>
+
+        <Button onClick={handleButtonClick}>
+          Меня часто приглашают сниматься бесплатно фотографы в обмен на рекламу
+        </Button>
+
+        <Collapse isCollapsed={isCollapsed}>
+          <h1 style={{  }}>
+            
+          </h1>
+        </Collapse>
+
+        <Button onClick={handleButtonClick}>Мне нужна каталожная предметная съемка, как можно сделать ее уникальной и интересной?</Button>
+
+        <Collapse isCollapsed={isCollapsed}>
+          <h1 style={{  }}>
+            
+          </h1>
         </Collapse>
       </div>
       <div>
